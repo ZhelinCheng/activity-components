0cfaf05fafedee2a1c72a3d85974d9f8f8f28c0f
ZhelinCheng <chengzhelin@foxmail.com>
2020-12-17 17:21:06 +0800

    feat: 新增内容

--- src/components/Turntable/index.tsx
+++ src/components/Turntable/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 // import cx from 'classnames'
 import './index.css'
 
@@ -10,27 +10,89 @@ import './index.css'
 
 interface TurntableProps {
   type?: 'circle' | 'rectangle'
-  time: number
-  rotate: number
+  duration?: number
+  rotate?: number
+  mode?: 'auto' | 'css' | 'canvas'
+  timingFunction?: 'ease' | 'ease-in' | 'ease-out' | 'ease-in-out'
+  backgroundImage: string
   // prizes: PrizeItem[]
 }
 
+function isTransition() {
+  console.log(111, typeof document)
+  if (typeof document !== 'object') {
+    return false
+  }
+  if (
+    document.body.style.webkitTransition !== undefined ||
+    document.body.style.transition !== undefined
+  ) {
+    return true
+  }
+  return false
+}
+
+function loadImage(src: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.src = src
+    img.onload = () => {
+      resolve(img)
+    }
+    img.onerror = (e) => {
+      reject(e)
+    }
+  })
+}
+
 export const Turntable = React.memo(
-  ({ type = 'circle', time, rotate }: TurntableProps): JSX.Element => {
+  ({
+    type = 'circle',
+    duration = 5,
+    rotate = 5,
+    mode = 'auto',
+    backgroundImage,
+    timingFunction = 'ease-out',
+  }: TurntableProps): JSX.Element => {
     const [startStatus, setStarStatus] = useState(0)
     const onStart = () => {
       setStarStatus(rotate)
     }
+    const modeActive = useMemo(() => {
+      return mode !== 'auto' ? mode : isTransition() ? 'css' : 'canvas'
+    }, [mode])
+
+    const styles =
+      modeActive === 'css'
+        ? {
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: '100%',
+            transition: `transform ${duration}s ${timingFunction}`,
+            transform: `rotate(${startStatus * 360}deg)`,
+          }
+        : {}
+
+    // 图片加载
+    useEffect(() => {
+      if (modeActive === 'canvas') {
+        const $canvas = document.getElementById('j-prize') as HTMLCanvasElement
+        const ctx = $canvas.getContext('2d') as CanvasRenderingContext2D
+        loadImage(backgroundImage).then((img) => {
+          ctx.drawImage(img, 0, 0)
+        })
+      }
+    }, [backgroundImage, modeActive])
 
     return (
       <div className="ac-turntable">
-        <div
-          style={{
-            transition: `transform ${time}s ease-out`,
-            transform: `rotate(${startStatus * 360}deg)`,
-          }}
-          className="ac-turntable__prize"
-        ></div>
+        {modeActive === 'css' ? (
+          <div style={styles} className="ac-turntable__prize"></div>
+        ) : (
+          <div style={styles} className="ac-turntable__prize">
+            <canvas id="j-prize"  />
+          </div>
+        )}
+
         <div className="ac-turntable__arrow"></div>
         <button onClick={onStart}>开始</button>
       </div>
--- src/examples/turntable/index.tsx
+++ src/examples/turntable/index.tsx
@@ -6,7 +6,10 @@ const TurntablePage = React.memo(
   (props: RouteComponentProps): JSX.Element => {
     return (
       <main>
-        <Turntable time={6} rotate={6} />
+        <Turntable
+          mode="canvas"
+          backgroundImage="https://cdn.jsdelivr.net/gh/ZhelinCheng/storage@master/picture/wR5hSB_44.png"
+        />
       </main>
     )
   }
